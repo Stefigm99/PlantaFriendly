4a4fd87d0587294138a0420f1febb6185f85570f
Stefigm99 <stefanygasca9@gmail.com>
2024-02-22 14:45:06 -0500

    Se crea correo automatico para la seccion de contactos, se elimina la barra de busqueda, y se organiza el texto del carrusel

--- js/main.js
+++ js/main.js
@@ -64,3 +64,26 @@ function mostrarTotalCarrito() {
   var cantidadCarrito = document.getElementById('cantidadCarrito');
   cantidadCarrito.textContent = cantidadEnCarrito.toString();
 }
+
+// Evento de correo seccion contacto
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'service_stefygm99';
+   const templateID = 'template_4ncuik8';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar';
+      alert('¡Tu mensaje ha sido enviado exitosamente!');
+    }, (err) => {
+      btn.value = 'Enviar';
+      alert(JSON.stringify(err));
+    });
+});
+
